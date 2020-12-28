2d2f79d75a07fc8b7a327fd251313bfb1cd664ec
Greg Carnie <greg.carnie@ibts.ca>
2020-12-27 23:10:06 -0700

    status page

---
+++ src/pages/status.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const StatusPage = () => {
+        return (
+        <>
+            <p>Site is up and running!</p>
+        </>
+    )
+}
+
+export default StatusPage
+
